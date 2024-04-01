49501b907482e171b1a7b54dca2c730b4b29ffd7
Ikram Chehouani <60435296+ikramchehouani@users.noreply.github.com>
2024-04-01 18:40:06 +0200

    added coins lambda

---
+++ lambda/coins_lambda.js
@@ -0,0 +1,80 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+  GetCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const dynamo = DynamoDBDocumentClient.from(client);
+const tableName = "coins";
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    switch (event.routeKey) {
+      case "DELETE /coins/{id}":
+        await dynamo.send(
+          new DeleteCommand({
+            TableName: tableName,
+            Key: {
+              id: event.pathParameters.id,
+            },
+          })
+        );
+        body = `Deleted item ${event.pathParameters.id}`;
+        break;
+      case "GET /coins/{id}":
+        const { Item } = await dynamo.send(
+          new GetCommand({
+            TableName: tableName,
+            Key: {
+              id: event.pathParameters.id,
+            },
+          })
+        );
+        body = Item;
+        break;
+      case "GET /coins":
+        const { Items } = await dynamo.send(
+          new ScanCommand({ TableName: tableName })
+        );
+        body = Items;
+        break;
+      case "POST /coins":
+        const requestJSON = JSON.parse(event.body);
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Item: {
+              id: requestJSON.id,
+              customer: requestJSON.customer,
+              amount: requestJSON.foodIds,
+              updatedAt: requestJSON.updatedAt
+            },
+          })
+        );
+        body = `Put coins ${requestJSON.id}`;
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+    return {
+      statusCode,
+      body,
+      headers,
+    };
+  }
+};
